2f1c622b8bfebc9313ef4da51724ac9efd472350
Arr0ws <fentimurliawati09@gmail.com>
2025-09-09 14:40:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     fs.readFile('./index.html', 'utf8', (err, html) => {
         if (err) {
-            return res.status(404).send("ERROR")
+            return res.status(500).send("ERROR")
         }
         res.send(html)
     })
